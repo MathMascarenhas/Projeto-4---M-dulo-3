aa44c6d52f6633694ea46abc5e585f36b097ff05
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-13 17:44:41 -0300

    fix(characters): middleware structure

--- src/characters/characters.route.js
+++ src/characters/characters.route.js
@@ -7,12 +7,12 @@ import {
   updateCharacter,
   deleteCharacter,
 } from './characters.controller.js';
-import { validId} from './src/characters/characters.middleware.js'
+import { validId } from './characters.middleware.js'
 
 router.get('/characters', findAll);
-router.get('/characters/find/:id', findById);
+router.get('/characters/find/:id', validId, findById);
 router.post('/characters/create', createCharacter);
-router.put('/characters/update/:id', updateCharacter);
-router.delete('/characters/delete/:id', deleteCharacter);
+router.put('/characters/update/:id', validId, updateCharacter);
+router.delete('/characters/delete/:id', validId, deleteCharacter);
 
 export default router;
