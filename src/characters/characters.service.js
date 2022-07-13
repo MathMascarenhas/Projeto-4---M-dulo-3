f6f785d278932c94beb858349bab4355fd8b7f2f
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-13 17:24:49 -0300

    fix(charactersController): fixing two functions

--- src/characters/characters.service.js
+++ src/characters/characters.service.js
@@ -1,15 +1,15 @@
-import Characters from './Characters.model.js'
+import Characters from './Characters.model.js';
 
 export const allCharacters = async () => await Characters.find();
 
 export const characterById = async (idParam) => await Characters.findById(idParam);
 
-export const createNewCharacter = async (character) => Characters.create(character);
+export const createNewCharacter = async (character) => await Characters.create(character);
 
 export const characterUpdate = async (idParam, updateBody) => {
-    return Characters.findByIdAndUpdate(idParam, updateBody).setOptions({
-        returnOriginal: false
-    });
+  return await Characters.findByIdAndUpdate(idParam, updateBody).setOptions({
+    returnOriginal: false,
+  });
 };
 
 export const removeCharacter = async (idParam) => await Characters.findByIdAndDelete(idParam);
