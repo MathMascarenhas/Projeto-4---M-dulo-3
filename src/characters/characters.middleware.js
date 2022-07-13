bbbc60ba9c53dc9de1489d3e3b640408bc38e56d
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-13 17:42:57 -0300

    build(characters): add veryfing id middleware

--- src/characters/characters.middleware.js
+++ src/characters/characters.middleware.js
@@ -1,3 +1,13 @@
 import mongoose from "mongoose";
 import Characters from "./Characters.model.js";
 
+export const validId = async (req, res, next) => {
+    const idParam = req.params.id;
+    if(!mongoose.Types.ObjectId.isValid(idParam)){
+        return res.status(404).send({message: 'ID inválido'})
+    }
+    if(await Characters.findById(idParam) === null){
+        return res.status(404).send({message: 'ID inválido!'})
+    }
+    next();
+}
