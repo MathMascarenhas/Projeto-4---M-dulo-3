1d3f2c6cb3940fa47bfff214ef96023b1af8a6ca
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-12 21:15:37 -0300

    build(index): building up Index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`),
+);
