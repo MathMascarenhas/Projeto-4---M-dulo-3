5b4329f85713cdcfc88bc5ea3c3be1515fca048f
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-14 17:36:51 -0300

    build(users)= add cryptography on user password

--- src/users/User.model.js
+++ src/users/User.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -29,6 +29,11 @@ const UserSchema = new mongoose.Schema({
 },{
     versionKey: false,
   });
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 const User = mongoose.model('User', UserSchema);
 export default User;
