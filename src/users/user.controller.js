ffcb507cd299c9af95d610e2a4009c2143c7ccd8
Matheus Mascarenhas <mathmasca@gmail.com>
2022-07-14 17:21:34 -0300

    feat(users): create find all users function

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -1,11 +1,19 @@
-import { createNewUser } from './user.service.js';
+import { createNewUser, findUsers } from './user.service.js';
 
 export const createUser = async (req, res) => {
     const user = await createNewUser(req.body).catch((err) => console.log(err.message));
-
     if(!user) {
         return res.status(400).send({ message: 'Erro ao criar o usuário!'});
     }
-
     res.status(201).send(user);
+}
+
+export const findAllUsers = async (req, res) => {
+    const users = await findUsers();
+
+    if(users.length === 0) {
+        return res.status(200).send({ message: "Não existem usuários cadastrados!"});
+    }
+    
+    res.send(users);
 }
--- src/users/user.service.js
+++ src/users/user.service.js
@@ -1,3 +1,5 @@
 import User from './User.model.js';
 
 export const createNewUser = async (body) => await User.create(body);
+
+export const findUsers = async () => await User.find();
